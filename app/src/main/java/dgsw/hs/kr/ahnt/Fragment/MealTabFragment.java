80cceec28fcc48c91f2fc67b3eadb90b8e09804e
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-03-23 10:37:27 +0900

    Meal Fragment NPE handled, UpdateUI Added

--- app/src/main/java/dgsw/hs/kr/ahnt/Fragment/MealTabFragment.java
+++ app/src/main/java/dgsw/hs/kr/ahnt/Fragment/MealTabFragment.java
@@ -23,35 +23,56 @@ import dgsw.hs.kr.ahnt.school.SchoolMenu;
  */
 public class MealTabFragment extends Fragment {
 
+    Calendar cal;
     SchoolMenu menu;
+
+    TextView tvMeal;
+    TextView tvCal;
 
     public MealTabFragment() {
         // Required empty public constructor
     }
 
-    public static MealTabFragment NewInstance(SchoolMenu menu){
+    public static MealTabFragment NewInstance(Calendar cal, SchoolMenu menu){
         MealTabFragment fragment = new MealTabFragment();
+        fragment.cal = cal;
         fragment.menu = menu;
         return fragment;
+    }
+
+    public void updateUI(Calendar cal, SchoolMenu menu){
+        this.cal = cal;
+        this.menu = menu;
+
+        if(menu != null){
+            if (tvMeal != null) {
+                tvMeal.setText(menu.toString());
+            }
+        }
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy년 MM월 dd일");
+        if (cal != null) {
+            if (tvCal != null) {
+                tvCal.setText(df.format(cal.getTime()));
+            }
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_meal_tab, container, false);
-        TextView tv = (TextView) view.findViewById(R.id.tvMeal);
-        tv.setText(menu.toString());
+        tvMeal = (TextView) view.findViewById(R.id.tvMeal);
+        tvCal = (TextView) view.findViewById(R.id.tvCalendar);
 
-        TextView tvCal = (TextView) view.findViewById(R.id.tvCalendar);
-        SimpleDateFormat df = new SimpleDateFormat("yyyy년 MM월 dd일");
-        tvCal.setText(df.format(menu.date.getTime()));
+        updateUI(cal, menu);
 
         tvCal.setOnClickListener((v) -> {
             MealActivity.DatePicker timePicker = new MealActivity.DatePicker();
             Bundle bundle = new Bundle();
-            bundle.putInt("year", menu.date.get(Calendar.YEAR));
-            bundle.putInt("month", menu.date.get(Calendar.MONTH));
-            bundle.putInt("day", menu.date.get(Calendar.DAY_OF_MONTH));
+            bundle.putInt("year", cal.get(Calendar.YEAR));
+            bundle.putInt("month", cal.get(Calendar.MONTH));
+            bundle.putInt("day", cal.get(Calendar.DAY_OF_MONTH));
 
             timePicker.setArguments(bundle);
             timePicker.show(getFragmentManager(), "날짜를 선택하세요");
@@ -59,6 +80,14 @@ public class MealTabFragment extends Fragment {
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+    public Calendar getCal() {
+        return cal;
+    }
+
+    public SchoolMenu getMenu() {
+        return menu;
     }
 
 }
