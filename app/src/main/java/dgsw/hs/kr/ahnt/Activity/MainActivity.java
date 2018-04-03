93cc16d19bd7a97f9332f52535249a6d222c689e
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-04-03 11:17:08 +0900

    add: main drawerview

--- app/src/main/java/dgsw/hs/kr/ahnt/Activity/MainActivity.java
+++ app/src/main/java/dgsw/hs/kr/ahnt/Activity/MainActivity.java
@@ -1,19 +1,43 @@
 package dgsw.hs.kr.ahnt.Activity;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.support.v7.widget.Toolbar;
 
 import dgsw.hs.kr.ahnt.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onBtnMealClicked(View view) {
