42fd442b1b5f1ade257fea3b1135701d48ba55e4
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-04-06 18:16:28 +0900

    Demo member model extends RealmObject

--- app/src/main/java/dgsw/hs/kr/ahnt/Model/Member.java
+++ app/src/main/java/dgsw/hs/kr/ahnt/Model/Member.java
@@ -1,9 +1,55 @@
 package dgsw.hs.kr.ahnt.Model;
 
+import io.realm.RealmObject;
+
 /**
  * Created by neutral on 02/04/2018.
  */
 
-public class Member {
+public class Member extends RealmObject{
+    private String id;
+    private String name;
+    private int grade;
+    private int classNum;
+    private int classCode;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(int classNum) {
+        this.classNum = classNum;
+    }
+
+    public int getClassCode() {
+        return classCode;
+    }
+
+    public void setClassCode(int classCode) {
+        this.classCode = classCode;
+    }
 }
