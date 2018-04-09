f6df01a7619c87698cade9dc168ef894c11c2031
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-04-09 09:47:00 +0900

    CalendarHelper's set was not needed.
    It was already in the api.

--- app/src/main/java/dgsw/hs/kr/ahnt/Helper/CalendarHelper.java
+++ app/src/main/java/dgsw/hs/kr/ahnt/Helper/CalendarHelper.java
@@ -14,37 +14,13 @@ public class CalendarHelper {
 
     public static Calendar CreateCalendar(int year, int month, int day) {
         Calendar cal = CreateCalendar();
-        set(cal, year, month, day, 0, 0, 0);
+        cal.set(year, month, day, 0, 0, 0);
         return cal;
     }
 
     public static Calendar CreateCalendar(int year, int month, int day, int hour, int min, int sec) {
         Calendar cal = CreateCalendar();
-        set(cal, year, month, day, hour, min, sec);
-
-        return cal;
-    }
-
-    public static Calendar set(Calendar cal, int year, int month, int day) {
-        cal.set(Calendar.YEAR, year);
-        cal.set(Calendar.MONTH, month);
-        cal.set(Calendar.DAY_OF_MONTH, day);
-
-        return cal;
-    }
-
-    public static Calendar set(Calendar cal, int year, int month, int day, int hour, int min, int sec) {
-        set(cal, year, month, day);
-        setTime(cal, hour, min, sec);
-
-        return cal;
-    }
-
-    public static Calendar setTime(Calendar cal, int hour, int min, int sec) {
-        cal.set(Calendar.HOUR_OF_DAY, hour);
-        cal.set(Calendar.MINUTE, min);
-        cal.set(Calendar.SECOND, sec);
-
+        cal.set(year, month, day, hour, min, sec);
         return cal;
     }
 }
