29f41ecfc24559c16f14b2745cd244fca1938011
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-04-10 09:20:09 +0900

    add: TokenInfo class

---
+++ app/src/main/java/dgsw/hs/kr/ahnt/Model/TokenInfo.java
@@ -0,0 +1,13 @@
+package dgsw.hs.kr.ahnt.Model;
+
+/**
+ * Created by neutral on 10/04/2018.
+ */
+
+public class TokenInfo {
+    public String token;
+
+    public TokenInfo(String token) {
+        this.token = token;
+    }
+}
