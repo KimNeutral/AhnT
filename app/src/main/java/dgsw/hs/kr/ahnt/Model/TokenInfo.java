f22b1e222c34d89c791a331321a2ce02e7d0477e
Neutral <rlaeodyd01@dgsw.hs.kr>
2018-05-06 00:21:25 +0900

    edit: TokenInfo extends RealmObject

--- app/src/main/java/dgsw/hs/kr/ahnt/Model/TokenInfo.java
+++ app/src/main/java/dgsw/hs/kr/ahnt/Model/TokenInfo.java
@@ -1,10 +1,12 @@
 package dgsw.hs.kr.ahnt.Model;
 
+import io.realm.RealmObject;
+
 /**
  * Created by neutral on 10/04/2018.
  */
 
-public class TokenInfo {
+public class TokenInfo extends RealmObject {
     public String token;
 
     public TokenInfo(String token) {
