a5b4dfc3af28d65f0228f745c7e47b6b4efeabef
KimDaeyong <01rlaeodyd@naver.com>
2018-06-11 14:46:13 +0900

    add: outfragment layout

--- app/src/main/java/kr/hs/dgsw/flow/Fragment/OutFragment.java
+++ app/src/main/java/kr/hs/dgsw/flow/Fragment/OutFragment.java
@@ -1,8 +1,37 @@
 package kr.hs.dgsw.flow.Fragment;
 
-public class OutFragment extends BaseFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import kr.hs.dgsw.flow.R;
+
+public class OutFragment extends BaseFragment {
     private static final String TITLE = "외출";
+
+    public OutFragment() {
+        // Required empty public constructor
+    }
+
+    public static OutFragment newInstance() {
+        OutFragment fragment = new OutFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_out, container, false);
+    }
 
     @Override
     public String getTitle() {
