898707b611cb62f674917fec7a6f3703a2a328b9
KimDaeyong <01rlaeodyd@naver.com>
2018-06-24 03:54:49 +0900

    edit: filled SharedPreferenceHelper

--- app/src/main/java/kr/hs/dgsw/flow/Helper/SharedPreferencesHelper.java
+++ app/src/main/java/kr/hs/dgsw/flow/Helper/SharedPreferencesHelper.java
@@ -1,4 +1,29 @@
 package kr.hs.dgsw.flow.Helper;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class SharedPreferencesHelper {
+
+    private static Context context;
+    private static final String SP_NAME = "PREF";
+
+    public static void setContext(Context context) {
+        SharedPreferencesHelper.context = context;
+    }
+
+    public static String getPreference(String key) {
+        if(context == null) return "";
+        SharedPreferences sp = context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
+        String val = sp.getString(key, "");
+        return val;
+    }
+
+    public static void setPreference(String key, String value) {
+        if(context == null) return;
+        SharedPreferences sp = context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor ed = sp.edit();
+        ed.putString(key, value);
+        ed.commit();
+    }
 }
